8f666588189c8ce8428658c5c35fdcd53f743e41
nathangroehl <79340234+nathangroehl@users.noreply.github.com>
2021-04-12 14:30:49 -0400

    added maintenance form and tele verification

--- vue/src/views/MyRental.vue
+++ vue/src/views/MyRental.vue
@@ -10,6 +10,14 @@
     <div>
       <transactionTile id="transactionTileId" v-bind:user="getPropertyObject" />
     </div>
+    <div>
+      <form class="formHolder">
+        <h1>Maintenance Request</h1>
+        <textarea class="textBox" name="description"></textarea>
+        <input type="submit" class="submit" name="" value="Submit">
+      </form>
+    <!-- <router-link class="btnHolder" :to="this.$store.state.currentSearchIndex"><button class= "backToSearch">Maintenance Request</button></router-link> -->
+    </div>  
   </body>
   </div>
 </template>
@@ -42,5 +50,27 @@ computed: {
 <style>
 #propertyTileId{
   margin-top: 8rem; 
+}
+.textBox{
+  width: 100%;
+  min-height: 5rem;
+  border-radius: .5rem;
+}
+
+h1{
+  font-size: 1.5em;
+  margin-top: 0;  
+}
+
+.formHolder {
+  background-color: #fff;
+  border-radius: 1.5rem;
+  overflow: hidden;
+  box-shadow: 0 3rem 6rem rgba(0, 0, 0, 0.589);
+  font-family: "Oswald", "Arial", "Helvetica", "sans-serif";
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  padding-right: 1.5rem;
 }
 </style>
