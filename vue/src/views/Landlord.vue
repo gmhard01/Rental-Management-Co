05ac920b4ac30b535b5f2cb8c16a6996fac045a8
nathangroehl <79340234+nathangroehl@users.noreply.github.com>
2021-04-14 11:31:16 -0400

    added toggle btn for new rental form in Landlord

--- vue/src/views/Landlord.vue
+++ vue/src/views/Landlord.vue
@@ -1,69 +1,70 @@
 <template>
     <div>
         <body>
-            <header>
-              <headerBar id="headerBarId" />
-            </header>
-            <div class=gridHolder>
-              <main id="propertyTileId">
-                <div v-for="property in getProperties" v-bind:key="property.propertyId">
-                  <propertyTile v-bind:property="property" />
-                  <propTransactions v-bind:property="property" id="propTransactions" />
-                </div>
-              </main>
+          <header>
+            <headerBar id="headerBarId" />
+          </header>
+          <div class=gridHolder>
+            <main id="propertyTileId">
+              <div v-for="property in getProperties" v-bind:key="property.propertyId">
+                <propertyTile v-bind:property="property" />
+                <propTransactions v-bind:property="property" id="propTransactions" />
+              </div>
+            </main>
+          </div>
+          <button class="showPayments" v-on:click='showNewRentalForm === false ? showNewRentalForm = true : showNewRentalForm = false'>Show/Hide New Rental Form</button>
+          <form class="newRentalForm" v-if='showNewRentalForm'>
+            <h2>Add a rental</h2>
+            <input id="rentalTitle" type="text" placeholder="Title" required>
+            <div class="addressForm">
+              <div>
+                <input type="number" placeholder="Street Number" required>
+                <input type="text" id="streetName" placeholder="Street Name" required>
+                <input type="text" placeholder="Unit Number">
+              </div>
+              <div>
+                <input type="text" placeholder="State Abbreviated" pattern="[A-Za-z]{2}" required>
+                <input type="text" placeholder="City" required>
+                <input type="text" id="county" placeholder="County">
+                <input type="text" placeholder="Zip Code" required>
+              </div>
             </div>
-            <form class="newRentalForm">
-              <h2>Add a rental</h2>
-              <input id="rentalTitle" type="text" placeholder="Title" required>
-              <div class="addressForm">
-                <div>
-                  <input type="number" placeholder="Street Number" required>
-                  <input type="text" id="streetName" placeholder="Street Name" required>
-                  <input type="text" placeholder="Unit Number">
-                </div>
-                <div>
-                  <input type="text" placeholder="State Abbreviated" pattern="[A-Za-z]{2}" required>
-                  <input type="text" placeholder="City" required>
-                  <input type="text" id="county" placeholder="County">
-                  <input type="text" placeholder="Zip Code" required>
-                </div>
+            <div class="bedAndBaths">
+              <input type="text" placeholder="Rent Amount" required>
+              <input type="number" placeholder="Number of Beds" required>
+              <input type="number" placeholder="Number of Baths" required>
+              <input type="number" placeholder="Square Footage">
+            </div>
+            <div class="inputPictures">
+                <input type="text" placeholder="Picture Url 1" required>
+                <input type="text" class="photoDesc" placeholder="Photo Description 1">
+                <input type="text" placeholder="Picture Url 2">
+                <input type="text" class="photoDesc" placeholder="Photo Description 2">
+            </div>
+            <div class="inputPictures">
+                <input type="text" placeholder="Picture Url 3">
+                <input type="text" class="photoDesc" placeholder="Photo Description 3">
+                <input type="text" placeholder="Picture Url 4">
+                <input type="text" class="photoDesc" placeholder="Photo Description 4">
+            </div>
+            <input type="date" placeholder="Available Date">
+            <textarea class="textBox" name="description" placeholder="Property description"></textarea>
+            <div class="formBtns">
+              <div>
+              <select name="propType" class="dropDownInput" required>
+                <option selected="House" value="House">House</option>
+                <option value="Apartment">Apartment</option>
+                <option value="Apartment">Townhome</option>
+                <option value="Apartment">Condo</option>
+              </select>
+              <select name="petsAllowed" class="dropDownInput" required>
+                <option selected="noPets" value="0">No pets</option>
+                <option value="pets">Pets Allowed</option>
+              </select>
               </div>
-              <div class="bedAndBaths">
-                <input type="text" placeholder="Rent Amount" required>
-                <input type="number" placeholder="Number of Beds" required>
-                <input type="number" placeholder="Number of Baths" required>
-                <input type="number" placeholder="Square Footage">
-              </div>
-              <div class="inputPictures">
-                  <input type="text" placeholder="Picture Url 1" required>
-                  <input type="text" class="photoDesc" placeholder="Photo Description 1">
-                  <input type="text" placeholder="Picture Url 2">
-                  <input type="text" class="photoDesc" placeholder="Photo Description 2">
-              </div>
-              <div class="inputPictures">
-                  <input type="text" placeholder="Picture Url 3">
-                  <input type="text" class="photoDesc" placeholder="Photo Description 3">
-                  <input type="text" placeholder="Picture Url 4">
-                  <input type="text" class="photoDesc" placeholder="Photo Description 4">
-              </div>
-              <input type="date" placeholder="Available Date">
-              <textarea class="textBox" name="description" placeholder="Property description"></textarea>
-              <div class="formBtns">
-                <div>
-                <select name="propType" class="dropDownInput" required>
-                  <option selected="House" value="House">House</option>
-                  <option value="Apartment">Apartment</option>
-                  <option value="Apartment">Townhome</option>
-                  <option value="Apartment">Condo</option>
-                </select>
-                <select name="petsAllowed" class="dropDownInput" required>
-                  <option selected="noPets" value="0">No pets</option>
-                  <option value="pets">Pets Allowed</option>
-                </select>
-                </div>
-                <input type="submit" class="submit" value="Add Rental"/> 
-              </div>             
-            </form>
+              <input type="submit" class="submit" value="Add Rental"/> 
+            </div>             
+          </form>
         </body>
     </div>
 </template>
@@ -79,6 +80,11 @@ export default {
     headerBar,
     propertyTile,
     //propTransactions
+  },
+    data() {
+    return {
+      showNewRentalForm: false,
+    }
   },
   created() {
     LandlordService.getPropertyList().then((response) => {
@@ -144,7 +150,7 @@ export default {
   width: 8rem;
 }
 #rentalTitle{
-  width: 13rem;
+  width: 16rem;
   font-size: 1.5rem;
 }
 
